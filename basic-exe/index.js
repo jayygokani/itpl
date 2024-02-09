550a63ad57b2b054110d03218f2ea9b9fba7f3ea
jayygokani <jay.gokani@improwised.com>
2024-02-09 17:39:22 +0530

    GH-push

--- basic-exe/index.js
+++ basic-exe/index.js
@@ -1,12 +1,13 @@
-function p(){
-    console.log("p function");
-    function c(a)
-    {
-        return 10;
-        // console.log("c fucntion");
-    }
-    // c();
-}
+let person = {
+    name: 'John',
+    age: 30,
+    city: 'New York',
+};
 
-// p();
-console.log(p())
+// Object.entries(person).forEach(([k, v]) => {
+//     console.log(`${k}: ${v}`);
+// });
+delete person.name
+console.log(Object.entries(person))
+console.log("values: " + Object.values(person))
+console.log("keys: " + Object.keys(person))
